5f97f319ddce4d3c850d8374c3101842145deaa0
MchevezS <saboriomichelle93@gmail.com>
2024-05-02 11:24:52 -0600

    ventana alert

--- java.js
+++ java.js
@@ -1,7 +1,10 @@
-let imagen = document.getElementById("imagen").onclick = function () {
-    mostrarMensaje()
+           let imagen = document.getElementById("imagen").onclick = function () {
+               mostrarMensaje()
 }
 
-function mostrarMensaje() {
-    alert("holaaa")
+     function mostrarMensaje() {
+         alert("Hola soy Michelle,tengo 19 años, me considero una persona amable, me gusta trabajar en equipo, me considdero una persona con demasiada paciencia. Me gusta practicar todo tipo de deporte, soy una persona que ama a los animales pero más a los perritos, gusta pasar mucho tiempo con mi familia, soy buena lider, soy una persona que sé escuchar alos demás.")
 }
+
+
+
